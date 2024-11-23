d6678db49b4f9fbe65437b4064fa7cf41293e120
Darren Ogola <darren.sunday@strathmore.edu>
2024-11-23 22:12:08 +0300

    My Day29 of 365 challange

---
+++ Day29.py
@@ -0,0 +1,9 @@
+def ip_address(address)#
+    new address = ""
+    split_address = address.split(".")
+    separator = "[.]"
+    new_address = separator. join(split_address)
+    return new_address
+IPad=input("Enter your IP address : ")
+ipaddress = ip_addressIPad)
+print (ipaddress)
